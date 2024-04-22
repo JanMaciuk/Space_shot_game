7170876b6392f5fe2fe36e71454dee0bb2d68d8a
JanMaciuk <38810823+JanMaciuk@users.noreply.github.com>
2024-04-22 21:13:46 +0200

    Generic sprite class with specific subclasses

--- GameName.py
+++ GameName.py
@@ -1,11 +1,13 @@
 import pygame
 from enum import Enum
+import random
 
 """
 Assets credits:
 Player: https://opengameart.org/content/skull-in-a-ufo-spacecraft
-Obstacle1: https://clipart-library.com/clip-art/168-1688863_clip-free-png-images-transparent-free-download-pngmart.htm
-Obstacle2: https://www.hiclipart.com/free-transparent-background-png-clipart-dplug
+Asteroid1: https://clipart-library.com/clip-art/168-1688863_clip-free-png-images-transparent-free-download-pngmart.htm
+Asteroid2: https://www.hiclipart.com/free-transparent-background-png-clipart-dplug
+Asteroid3: https://www.pngwing.com/en/free-png-zvitc
 """
 
 # Variables and constants
@@ -15,7 +17,9 @@ BACKGROUND_COLOR = (0, 0, 0) # Black, we are in space
 PLAYER_RESCALE = 30
 MOVEMENT_SPEED = 3
 FPS = 60
-OBSTACKLE_COUNT = 5 # The number of obstacles on the screen
+ASTEROID_COUNT = 5 # The number of asteroids on the screen
+ASTEROID_SPRITES = ["Assets\Asteroid1.png", "Assets\Asteroid2.png", "Assets\Asteroid3.png"]
+ASTEROID_RESCALE = 20
 ENEMY_COUNT = 2 # The number of enemies on the screen
 AMMO_COUNT = 1 # The number of collectable ammo boxes on the screen
 main_loop = True
@@ -33,14 +37,12 @@ class object_type(Enum):
     BULLET = 4    # Maybe?
     EXPLOSION = 5 # Maybe?
 
-class playerSprite(pygame.sprite.Sprite):
+class genericSprite(pygame.sprite.Sprite):
+    """
+    Generic class for all sprites with basic functionalities that are not type-specific
+    """
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("Assets\Player.png").convert_alpha()
-        self.rect = self.image.get_rect()
-        self.image = pygame.transform.scale(self.image, (self.rect.width//PLAYER_RESCALE, self.rect.height//PLAYER_RESCALE))
-        self.rect = self.image.get_rect()   # Get new rect after rescaling
-        self.rect.center = (SCREEN_WIDTH//2, SCREEN_HEIGHT-self.rect.height) # Start at the bottom center
 
     def draw(self):
         screen.blit(self.image, self.rect)
@@ -50,6 +52,43 @@ class playerSprite(pygame.sprite.Sprite):
 
     def moveRight(self):
         self.rect.x += MOVEMENT_SPEED
+
+    def moveDown(self):
+        self.rect.y += MOVEMENT_SPEED
+    
+    def positionUp(self, resetX = False):
+        # Bring the sprite to the top, slighty above the frame.
+        self.rect.y = -self.rect.height
+        # Move object to random x position
+        if resetX: 
+            self.rect.x = random.randint(0, SCREEN_WIDTH-self.rect.width)
+        # Asteroids have a chance to change their picture
+        if self.isinstance(asteroidSprite):
+            self.reRollPicture()
+
+class asteroidSprite(genericSprite):
+    def __init__(self):
+        super().__init__()
+        self.reRollPicture()
+
+    def reRollPicture(self):
+        #Change the sprite to a random asteroid
+        imageIndex = random.randint(0, len(ASTEROID_SPRITES)-1)
+        self.image = pygame.image.load(ASTEROID_SPRITES[imageIndex]).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.image = pygame.transform.scale(self.image, (self.rect.width//ASTEROID_RESCALE, self.rect.height//ASTEROID_RESCALE))
+        self.rect = self.image.get_rect()   # Get new rect after rescaling
+
+
+class playerSprite(genericSprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Assets\Player.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.image = pygame.transform.scale(self.image, (self.rect.width//PLAYER_RESCALE, self.rect.height//PLAYER_RESCALE))
+        self.rect = self.image.get_rect()   # Get new rect after rescaling
+        self.rect.center = (SCREEN_WIDTH//2, SCREEN_HEIGHT-self.rect.height) # Start at the bottom center
+
     
 player = playerSprite()
 player.draw()
@@ -68,7 +107,6 @@ while main_loop:
     if pressed_keys[pygame.K_RIGHT] and player.rect.x < SCREEN_WIDTH - player.rect.width:
         player.moveRight()
     
-
 
     # Draw a new frame
     screen.fill(BACKGROUND_COLOR)
