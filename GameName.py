5ba900b4e7ef1792d27c54cd5701ce11a5fbf5a0
JanMaciuk <38810823+JanMaciuk@users.noreply.github.com>
2024-05-19 19:08:24 +0200

    Move main game logic into a class

--- GameName.py
+++ GameName.py
@@ -43,64 +43,122 @@ DEFAULT_PROFILE_PATH = "default.json"
 BACKGROUND_IMAGES = [r"Assets\Bg1.jpg", r"Assets\Bg2.jpg", r"Assets\Bg3.jpeg"]
 BACKGROUND_IMAGE = pygame.image.load(BACKGROUND_IMAGES[random.randint(0, len(BACKGROUND_IMAGES)-1)])
 BACKGROUND_IMAGE = pygame.transform.scale(BACKGROUND_IMAGE, (SCREEN_WIDTH, SCREEN_HEIGHT))
-mainLoop = True
-profileFilePath = DEFAULT_PROFILE_PATH
-profileDict = None
-
-#Try reading the profile from command line argument
-def tryReadProfile() -> bool:
-    global profileDict
-    try:
-        profileDict = json.load(open(profileFilePath, "r"))
-        for key in ["Score", "Health", "Ammo", "EnemySpeed", "EnemyHealth", "EnemyCount", "AsteroidCount", "SupplyCount"]:
-            if key not in profileDict or not isinstance(profileDict[key], int):
-                raise AssertionError()
-    except:
-        return False
-    return True
 
 
+class Game():
+    def __init__(self) -> None:
+        self.doLoop = True
+        self.profileFilePath = DEFAULT_PROFILE_PATH
+        self.profileDict = None
+        # Try to read the profile from the command line argument
+        if len(sys.argv) > 1: 
+            self.profileFilePath = sys.argv[1]
+            if not self.tryReadProfile():
+                self.profileFilePath = DEFAULT_PROFILE_PATH
 
-# Try to read the profile from the command line argument
-if len(sys.argv) > 1: 
-    profileFilePath = sys.argv[1]
-    if not tryReadProfile():
-        profileFilePath = DEFAULT_PROFILE_PATH
+        # If the profile was not loaded, use the default settings
+        if not self.profileDict:
+            print("No valid profile path passed, using default settings.")
+            if not self.tryReadProfile():
+                    # Game cannot run without settings, unrecoverable error
+                    raise FileNotFoundError("Unable to read default profile file, verify the file exists and is correctly formatted.")
+        # Load the profile settings:
+        self.playerScore = self.profileDict["Score"]
+        self.PLAYER_HEALTH = self.profileDict["Health"]
+        self.PLAYER_AMMO = self.profileDict["Ammo"]
+        self.ENEMY_TRACKING_SPEED = self.profileDict["EnemySpeed"]
+        self.ENEMY_HEALTH = self.profileDict["EnemyHealth"]
+        self.ASTEROID_DURABILITY = self.profileDict["AsteroidDurability"]
+        self.ENEMY_COUNT = self.profileDict["EnemyCount"]
+        self.ASTEROID_COUNT = self.profileDict["AsteroidCount"]
+        self.SUPPLY_COUNT = self.profileDict["SupplyCount"]
+        # Initialize the game
+        pygame.init()
+        pygame.display.set_caption("Game Name")
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.allSprites = pygame.sprite.Group()
+        # Initialize the game sprites
+        self.player = playerSprite(self)
+        self.player.draw()
+        self.missile = missileSprite(self)
+        self.missile.draw()
+        self.allSprites.add(self.missile)
+        self.allSprites.add(self.player)
+        for _ in range(self.ASTEROID_COUNT): self.allSprites.add(asteroidSprite(self))
+        for _ in range(self.ENEMY_COUNT): self.allSprites.add(enemySprite(self))
+        self.mainLoop()
+    
+    def tryReadProfile(self) -> bool:
+        try:
+            self.profileDict = json.load(open(self.profileFilePath, "r"))
+            for key in ["Score", "Health", "Ammo", "EnemySpeed", "EnemyHealth", "EnemyCount", "AsteroidCount", "SupplyCount"]:
+                if key not in self.profileDict or not isinstance(self.profileDict[key], int):
+                    raise AssertionError()
+        except:
+            return False
+        return True
+    
+    def mainLoop(self) -> None:
+        while self.doLoop:
+            # Check for game exit command
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                    self.saveProfileQuit()
 
-# If the profile was not loaded, use the default settings
-if not profileDict:
-    print("No valid profile path passed, using default settings.")
-    if not tryReadProfile():
-            # Game cannot run without settings, unrecoverable error
-            raise FileNotFoundError("Unable to read default profile file, verify the file exists and is correctly formatted.")
+            # Check for player actions
+            pressed_keys = pygame.key.get_pressed()
+            if pressed_keys[pygame.K_LEFT] and self.player.rect.x > 0:
+                self.player.moveLeft()
+            if pressed_keys[pygame.K_RIGHT] and self.player.rect.x < SCREEN_WIDTH - self.player.rect.width:
+                self.player.moveRight()
+            if pressed_keys[pygame.K_SPACE]:
+                self.player.fireMissile()
 
-# Load the profile settings
-playerScore = profileDict["Score"]
-PLAYER_HEALTH = profileDict["Health"]
-PLAYER_AMMO = profileDict["Ammo"]
-ENEMY_TRACKING_SPEED = profileDict["EnemySpeed"]
-ENEMY_HEALTH = profileDict["EnemyHealth"]
-ASTEROID_DURABILITY = profileDict["AsteroidDurability"]
-ENEMY_COUNT = profileDict["EnemyCount"]
-ASTEROID_COUNT = profileDict["AsteroidCount"]
-SUPPLY_COUNT = profileDict["SupplyCount"]
+            # Move all other sprites down and check if they collided with the player:
+            for sprite in self.allSprites:
+                if sprite != self.player:
+                    sprite.moveDown()
+                    if sprite.rect.y > SCREEN_HEIGHT:
+                        sprite.positionUp()
+                    if sprite.rect.colliderect(self.player.rect):
+                        print("Something collided with the player")
+                        self.player.takeDamage()
+                        sprite.positionUp()
+            
+            #Check for missile hit:
+            for sprite in self.allSprites:
+                if sprite != self.missile and sprite != self.player:
+                    if self.missile.rect.colliderect(sprite.rect):
+                        sprite.takeDamage()
+                        self.missile.positionUp()
 
-# Initialize the game
-pygame.init()
-pygame.display.set_caption("Game Name")
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-allSprites = pygame.sprite.Group()
+            # Draw a new frame
+            self.screen.blit(BACKGROUND_IMAGE, (0, 0))
+            self.allSprites.draw(self.screen)
+            pygame.time.Clock().tick(FPS)
+            pygame.display.update()
+
+    def saveProfileQuit(self) -> None:
+        ''' 
+        Save the current results and settings to the profile file (if its not default)
+        Then open the main menu passing current profile path as argument
+        '''
+        self.doLoop = False    # Exit the game loop
+        #TODO: Save the current results to the profile file
+        pygame.quit()
+        sys.exit()
 
 class genericSprite(pygame.sprite.Sprite):
     """
     Generic class for all sprites with basic functionalities that are not type-specific
     """
-    def __init__(self) -> None:
+    def __init__(self, gameInstance) -> None:
         super().__init__()
         self.health = 1
+        self.GI = gameInstance
 
     def draw(self) -> None:
-        screen.blit(self.image, self.rect)
+        self.GI.screen.blit(self.image, self.rect)
 
     def moveLeft(self) -> None:
         self.rect.x -= MOVEMENT_SPEED
@@ -131,19 +189,19 @@ class genericSprite(pygame.sprite.Sprite):
         # Move object to random x position
         self.rect.x = random.randint(0, SCREEN_WIDTH-self.rect.width)
         # If the move caused a collision, try again
-        while len(pygame.sprite.spritecollide(self, allSprites, False)) > 1:
+        while len(pygame.sprite.spritecollide(self, self.GI.allSprites, False)) > 1:
             self.rect.x = random.randint(0, SCREEN_WIDTH-self.rect.width)
             self.rect.y = -self.rect.height - random.randint(0, SCREEN_HEIGHT)
         
 class asteroidSprite(genericSprite):
-    def __init__(self) -> None:
-        super().__init__()
-        self.health = ASTEROID_DURABILITY
+    def __init__(self, gameInstance) -> None:
+        super().__init__(gameInstance)
+        self.health = self.GI.ASTEROID_DURABILITY
         self.positionUp() # Start at the top of the screen and choose a sprite picture
 
     def reRollPicture(self) -> None:
         #Change the sprite to a random asteroid
-        self.health = ASTEROID_DURABILITY  # Reset health as the asteroid respawns
+        self.health = self.GI.ASTEROID_DURABILITY  # Reset health as the asteroid respawns
         imageIndex = random.randint(0, len(ASTEROID_SPRITES)-1)
         self.image = pygame.image.load(ASTEROID_SPRITES[imageIndex]).convert_alpha()
         self.rect = self.image.get_rect()
@@ -151,14 +209,14 @@ class asteroidSprite(genericSprite):
         self.rect = self.image.get_rect()   # Get new rect after rescaling
 
 class enemySprite(genericSprite):
-    def __init__(self) -> None:
-        super().__init__()
-        self.health = ENEMY_HEALTH
+    def __init__(self, gameInstance) -> None:
+        super().__init__(gameInstance)
+        self.health = self.GI.ENEMY_HEALTH
         self.positionUp() # Start at the top of the screen and choose a sprite picture
 
     def reRollPicture(self) -> None:
         #Change the sprite to a random enemy
-        self.health = ENEMY_HEALTH  # Reset health as the enemy respawns
+        self.health = self.GI.ENEMY_HEALTH  # Reset health as the enemy respawns
         imageIndex = random.randint(0, len(ENEMY_SPRITES)-1)
         self.image = pygame.image.load(ENEMY_SPRITES[imageIndex]).convert_alpha()
         self.rect = self.image.get_rect()
@@ -167,30 +225,30 @@ class enemySprite(genericSprite):
 
     def moveDown(self) -> None:
         self.rect.y += MOVEMENT_SPEED
-        if self.rect.x < player.rect.x-2 and not len(pygame.sprite.spritecollide(self, allSprites, False)) > 1:
-            self.rect.x += ENEMY_TRACKING_SPEED
-        elif self.rect.x > player.rect.x+2 and not len(pygame.sprite.spritecollide(self, allSprites, False)) > 1:
-            self.rect.x -= ENEMY_TRACKING_SPEED
+        if self.rect.x < self.GI.player.rect.x-2 and not len(pygame.sprite.spritecollide(self, self.GI.allSprites, False)) > 1:
+            self.rect.x += self.GI.ENEMY_TRACKING_SPEED
+        elif self.rect.x > self.GI.player.rect.x+2 and not len(pygame.sprite.spritecollide(self, self.GI.allSprites, False)) > 1:
+            self.rect.x -= self.GI.ENEMY_TRACKING_SPEED
 
 class playerSprite(genericSprite):
-    def __init__(self) -> None:
-        super().__init__()
-        self.health = PLAYER_HEALTH
+    def __init__(self, gameInstance) -> None:
+        super().__init__(gameInstance)
+        self.health = self.GI.PLAYER_HEALTH
         self.image = pygame.image.load(r"Assets\Player.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.image = pygame.transform.scale(self.image, (self.rect.width//PLAYER_RESCALE, self.rect.height//PLAYER_RESCALE))
         self.rect = self.image.get_rect()   # Get new rect after rescaling
         self.rect.center = (SCREEN_WIDTH//2, SCREEN_HEIGHT-self.rect.height) # Start at the bottom center
-        self.ammo = PLAYER_AMMO
+        self.ammo = self.GI.PLAYER_AMMO
 
     def reRollPicture(self) -> None:
         # If this was called on the player, it means he died.
         self.health = 0  # Zero health means a dead player.
-        saveProfileQuit()
+        self.GI.saveProfileQuit()
 
     def fireMissile(self) -> bool:
-        if self.ammo > 0 and missile.rect.y < 0:
-            missile.setAbovePlayer()
+        if self.ammo > 0 and self.GI.missile.rect.y < 0:
+            self.GI.missile.setAbovePlayer()
             self.ammo -= 1
             return True
         return False
@@ -199,8 +257,8 @@ class playerSprite(genericSprite):
         pass #TODO: Implement player damage
 
 class missileSprite(genericSprite):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, gameInstance) -> None:
+        super().__init__(gameInstance)
         self.image = pygame.image.load(r"Assets\Missile.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.positionUp() # Start above the screen, invisible
@@ -210,72 +268,6 @@ class missileSprite(genericSprite):
 
     def setAbovePlayer(self) -> None:
         ''' Position missle above player '''
-        self.rect.center = (player.rect.centerx, player.rect.centery - player.rect.height)
+        self.rect.center = (self.GI.player.rect.centerx, self.GI.player.rect.centery - self.GI.player.rect.height)
 
-
-def saveProfileQuit() -> None:
-    ''' 
-    Save the current results and settings to the profile file (if its not default)
-    Then open the main menu passing current profile path as argument
-    '''
-    global mainLoop
-    mainLoop = False    # Exit the game loop
-    #TODO: Save the current results to the profile file
-
-
-# Initialize the game sprites
-player = playerSprite()
-player.draw()
-missile = missileSprite()
-missile.draw()
-allSprites.add(missile)
-allSprites.add(player)
-for _ in range(ASTEROID_COUNT): allSprites.add(asteroidSprite())
-for _ in range(ENEMY_COUNT): allSprites.add(enemySprite())
-
-
-# Begin main game loop
-while mainLoop:
-    # Check for game exit command
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-            saveProfileQuit()
-
-    # Check for player actions
-    pressed_keys = pygame.key.get_pressed()
-    if pressed_keys[pygame.K_LEFT] and player.rect.x > 0:
-        player.moveLeft()
-    if pressed_keys[pygame.K_RIGHT] and player.rect.x < SCREEN_WIDTH - player.rect.width:
-        player.moveRight()
-    if pressed_keys[pygame.K_SPACE]:
-        player.fireMissile()
-
-    # Move all other sprites down and check if they collided with the player:
-    for sprite in allSprites:
-        if sprite != player:
-            sprite.moveDown()
-            if sprite.rect.y > SCREEN_HEIGHT:
-                sprite.positionUp()
-            if sprite.rect.colliderect(player.rect):
-                print("Something collided with the player")
-                player.takeDamage()
-                sprite.positionUp()
-    
-    #Check for missile hit:
-    for sprite in allSprites:
-        if sprite != missile and sprite != player:
-            if missile.rect.colliderect(sprite.rect):
-                sprite.takeDamage()
-                missile.positionUp()
-
-
-    
-
-    # Draw a new frame
-    screen.blit(BACKGROUND_IMAGE, (0, 0))
-    allSprites.draw(screen)
-    pygame.time.Clock().tick(FPS)
-    pygame.display.update()
-
-
-pygame.quit()
+Game()
